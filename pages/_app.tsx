09043a3497a12c976933d8e30003a117a4c2b40f
danny-thet <thetnaingoosru@gmail.com>
2022-09-08 20:28:58 +0700

    fix: hydration issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+	const [isClient, setIsClient] = useState(false);
+
+	useEffect(() => {
+		setIsClient(true);
+	}, []);
+
+	if (!isClient) {
+		return <div>loading...</div>;
+	}
+
 	return (
 		<ChakraProvider>
 			<Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,19 @@
 import type { NextPage } from "next";
+import { useEffect, useState } from "react";
 import { ColorGuessingMain } from "../src/components/ColorGuessingMain";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+	const [isClient, setIsClient] = useState(false);
+
+	useEffect(() => {
+		setIsClient(true);
+	}, []);
+
+	if (!isClient) {
+		return <div>loading...</div>;
+	}
+
 	return (
 		<main className={styles.main}>
 			<ColorGuessingMain />;
